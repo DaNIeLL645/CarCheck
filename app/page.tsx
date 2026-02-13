ae817fbcc8666fdc2ec337407c15041fc07cf5bf
Daniel <danielcraciun485@gmail.com>
2026-02-13 19:54:44 +0200

    Setup: Clean install Next.js

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div className="p-10">
+      <h1 className="text-3xl font-bold text-blue-600">
+        CarCheck - Platforma de Inspecție
+      </h1>
+      <p>Bine ai venit în prima ta aplicație Next.js!</p>
+    </div>
+  );
+}
